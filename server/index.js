9c84a2022a371ab45776bf5b3684d96081320224
=amartinn <martoy123691@gmail.com>
2021-02-28 16:55:56 +0200

    initial commit

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const config = require('./config/config')
+const dbConnection = require('./config/database')
+
+const app = express()
+
+dbConnection().then(() => {
+
+  require('./config/express')(app)
+  // require('./config/routes')(app)
+
+  app.listen(config.port, console.log(`Listening on port ${config.port}!`))
+}).catch(err => console.error(err))
