e9abcce80719f38325f16b0b129ef485d3ff1365
=amartinn <martoy123691@gmail.com>
2021-02-28 15:51:03 +0200

    removed unnecessary imports/fix warnings

--- client/src/components/languageDropdown/languageDropdown.js
+++ client/src/components/languageDropdown/languageDropdown.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { Select } from "@material-ui/core";
 import * as ACTIONS from "../../actions/language";
 import { bindActionCreators } from "redux";
